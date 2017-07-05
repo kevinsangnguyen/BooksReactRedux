0c0d20a9dcb7bed9a3f24bb8f5b90fbe7e7b553e
kevinsangnguyen <kevinsangnguyen@gmail.com>
2017-07-05 14:31:13 -0700

    books reducer added

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'JavaScript For Beginners'},
+    { title: 'Harry Potter'},
+    { title: 'The Dark Tower'},
+    { title: 'Eloquent Ruby'},
+  ]
+}
